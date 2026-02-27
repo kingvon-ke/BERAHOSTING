56fd527212e255613693d517382e5dca1c7821f6
kingvon-ke <cloudtech.bera@gmail.com>
2026-02-27 14:01:33 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <strictmode>
-    <app/>
+  <StrictMode>    {/* ✅ Capitalized - matches import */}
+    <App />       {/* ✅ Capitalized - matches import */}
   </StrictMode>,
 );
